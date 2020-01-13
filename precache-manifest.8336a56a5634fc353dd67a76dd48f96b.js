51cef00ccc398aa2a33f730c3f96ca256c692ac3
poengen <pal.oskar.engen@gmail.com>
2020-01-13 09:13:47 +0100

    Updates

---
+++ precache-manifest.8336a56a5634fc353dd67a76dd48f96b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3fdb861cced8a0920a1bf0fba60587",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "c004c34320b31e1eb5e5",
+    "url": "/songs/static/js/2.fb20771b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/songs/static/js/2.fb20771b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fe6c32fc9d745c80925",
+    "url": "/songs/static/js/main.0a257db6.chunk.js"
+  },
+  {
+    "revision": "96648b8beca73b037d95",
+    "url": "/songs/static/js/runtime-main.4738303f.js"
+  }
+]);
